80d0e8c77359445c84d989abc2ef372c3c9af745
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-07-03 11:06:06 +0100

    refactor: Update User model in User.ts

--- src/model/User.ts
+++ src/model/User.ts
@@ -65,3 +65,7 @@ const UserSchema: Schema<User> = new Schema({
     messages: [MessageSchema]
 })
 
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || (mongoose.model<User>("User"), UserSchema)
+
+export default UserModel;
