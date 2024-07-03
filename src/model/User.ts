902222d06206afaae7c0ff7542b05f71be79e4da
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-07-03 10:50:04 +0100

    chore: Add Message model to User.ts

--- src/model/User.ts
+++ src/model/User.ts
@@ -5,3 +5,15 @@ export interface Message extends Document {
     content: string;
     createdAt: Date;
 }
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
