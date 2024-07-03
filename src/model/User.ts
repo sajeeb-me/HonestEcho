76368d1230587fddef7adb71bdf6228d9eb3c82d
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-07-03 11:02:11 +0100

    refactor: Add User model to User.ts

--- src/model/User.ts
+++ src/model/User.ts
@@ -6,6 +6,17 @@ export interface Message extends Document {
     createdAt: Date;
 }
 
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+    isAcceptingMessage: boolean;
+    messages: Message[];
+}
+
 const MessageSchema: Schema<Message> = new Schema({
     content: {
         type: String,
@@ -17,3 +28,40 @@ const MessageSchema: Schema<Message> = new Schema({
         default: Date.now
     }
 })
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Please use a valid email address']
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Verify code is required"],
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify code expiry is required"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true
+    },
+    messages: [MessageSchema]
+})
+
