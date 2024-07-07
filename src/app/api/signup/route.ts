c62694675cb8450612b57065b688c10911f8dbce
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-07-07 09:47:08 +0100

    fix: Fix missing semicolons in signup route

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -8,7 +8,7 @@ export async function POST(request: Request) {
     await dbConnect();
 
     try {
-        const { username, email, password } = await request.json()
+        const { username, email, password } = await request.json();
 
         const existingUserVerifiedByUsername = await UserModel.findOne({
             username,
@@ -25,8 +25,8 @@ export async function POST(request: Request) {
             )
         }
 
-        const existingUserByEmail = await UserModel.findOne({ email })
-        const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
+        const existingUserByEmail = await UserModel.findOne({ email });
+        let verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
 
         if (existingUserByEmail) {
             if (existingUserByEmail.isVerified) {
@@ -36,7 +36,7 @@ export async function POST(request: Request) {
                         message: "User already exist with this email"
                     },
                     { status: 400 }
-                )
+                );
             } else {
                 const hashedPassword = await bcrypt.hash(password, 10);
                 existingUserByEmail.password = hashedPassword;
@@ -47,7 +47,7 @@ export async function POST(request: Request) {
             }
         } else {
             const hashedPassword = await bcrypt.hash(password, 10);
-            const expiryDate = new Date()
+            const expiryDate = new Date();
             expiryDate.setHours(expiryDate.getHours() + 1)
 
             const newUser = new UserModel({
