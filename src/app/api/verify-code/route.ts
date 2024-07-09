ead2dbf643fb33518d30039b95ad4236612238f8
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-07-09 17:13:05 +0100

    chore: Enable NEXTAUTH_SECRET in CI workflow and fix code verification logic

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -21,7 +21,7 @@ export async function POST(request: Request) {
             )
         }
 
-        const isCodeValid = user.verifyCode == code.verifyCode;
+        const isCodeValid = user.verifyCode == code;
         const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date()
 
         if (isCodeValid && isCodeNotExpired) {
