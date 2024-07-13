706357631e89810dae8b57e839d865c70e680cb4
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-07-13 11:31:48 +0100

    chore: Add lazy loading for images and update npm dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <AuthProvider>
         <body className={inter.className}>{children}</body>
+        <Toaster />
       </AuthProvider>
     </html>
   );
