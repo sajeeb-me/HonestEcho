02692bec3fbf68ae213357dabf299cbfa2899995
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-07-16 10:37:20 +0100

    chore: Update npm dependencies to include dayjs library

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -26,6 +26,7 @@ import { Message } from "@/model/User"
 import { useToast } from "./ui/use-toast"
 import axios from "axios"
 import { ApiResponse } from "@/types/ApiResponse"
+import dayjs from 'dayjs';
 
 type MessageCardProps = {
     message: Message;
@@ -33,6 +34,7 @@ type MessageCardProps = {
 }
 
 const MessageCard = ({ message, onMessageDelete }: MessageCardProps) => {
+    console.log("message", message)  // TODO: Delete this line
     const { toast } = useToast();
     const handleDeleteConfirm = async () => {
         const response = await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
@@ -45,41 +47,40 @@ const MessageCard = ({ message, onMessageDelete }: MessageCardProps) => {
     }
 
     return (
-        <Card>
+        <Card className="card-bordered">
             <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-
-                <AlertDialog>
-                    <AlertDialogTrigger asChild>
-                        <Button variant="destructive">
-                            <X className="w-5 h-5" />
-                        </Button>
-                    </AlertDialogTrigger>
-                    <AlertDialogContent>
-                        <AlertDialogHeader>
-                            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                            <AlertDialogDescription>
-                                This action cannot be undone. This will permanently delete your
-                                account and remove your data from our servers.
-                            </AlertDialogDescription>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter>
-                            <AlertDialogCancel>Cancel</AlertDialogCancel>
-                            <AlertDialogAction onClick={handleDeleteConfirm}>
-                                Continue
-                            </AlertDialogAction>
-                        </AlertDialogFooter>
-                    </AlertDialogContent>
-                </AlertDialog>
-
-                <CardDescription>Card Description</CardDescription>
+                <div className="flex justify-between items-center">
+                    <CardTitle>{message.content}</CardTitle>
+                    <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <Button variant='destructive'>
+                                <X className="w-5 h-5" />
+                            </Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    This action cannot be undone. This will permanently delete
+                                    this message.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>
+                                    Cancel
+                                </AlertDialogCancel>
+                                <AlertDialogAction onClick={handleDeleteConfirm}>
+                                    Continue
+                                </AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                </div>
+                <div className="text-sm">
+                    {dayjs(message.createdAt).format('MMM D, YYYY h:mm A')}
+                </div>
             </CardHeader>
-            {/* <CardContent>
-                <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
-            </CardFooter> */}
+            <CardContent></CardContent>
         </Card>
 
     )
