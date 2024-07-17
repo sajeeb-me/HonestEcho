9f8cfe3a14a73c02404b7328e11362e883a06043
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-07-17 11:29:59 +0100

    Refactor Navbar component to improve accessibility and add user greeting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,18 +13,26 @@ const Navbar = () => {
     const user: User = session?.user as User;
 
     return (
-        <nav className='p-4 md:p-6 shadow-md'>
+        <nav className='p-4 md:p-6 shadow-md bg-gray-900 text-white'>
             <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
-                <a className='text-xl font-bold mb-4 md:mb-0' href="#">HonestEcho</a>
+                <a className='text-xl font-bold mb-4 md:mb-0' href="/">HonestEcho</a>
                 {
                     session ? (
                         <>
-                            <span className='mr-4'>Welcome, {user?.username || user?.email}</span>
-                            <Button className='w-full md:w-auto' onClick={() => signOut()} >Logout</Button>
+                            <span className='mr-4'>
+                                Hello, {user?.username || user?.email}
+                            </span>
+                            <Button
+                                className='w-full md:w-auto'
+                                variant={'secondary'}
+                                onClick={() => signOut()}
+                            >
+                                Logout
+                            </Button>
                         </>
                     ) : (
                         <Link href='/signin'>
-                            <Button className='w-full md:w-auto'>Login</Button>
+                            <Button className='w-full md:w-auto' variant={'secondary'}>Login</Button>
                         </Link>
                     )
                 }
