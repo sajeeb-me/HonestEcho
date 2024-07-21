e0180c326fe1e1cee6c2ea600da329d3ffdc76dc
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-07-21 11:57:56 +0100

    Refactor sign-in and sign-up pages to update heading font size

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -62,7 +62,7 @@ export default function SignIn() {
         <div className="flex justify-center items-center min-h-screen bg-gray-800">
             <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
                 <div className="text-center">
-                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+                    <h1 className="text-3xl font-extrabold tracking-tight lg:text-5xl mb-6">
                         Welcome Back to HonestEcho
                     </h1>
                     <p className="mb-4">Sign in to continue your honest secret conversations</p>
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -87,7 +87,7 @@ const SignUp = () => {
         <div className="flex justify-center items-center min-h-screen bg-gray-800">
             <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
                 <div className="text-center">
-                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+                    <h1 className="text-3xl font-extrabold tracking-tight lg:text-5xl mb-6">
                         Join HonestEcho
                     </h1>
                     <p className="mb-4">Sign up to start your honest anonymous adventure</p>
